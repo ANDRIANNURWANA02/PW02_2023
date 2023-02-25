58f1caf13d2a4ae4449adeb64cac803269dc02af
ANDRIANNURWANA02 <114549849+ANDRIANNURWANA02@users.noreply.github.com>
2023-02-25 14:31:39 +0700

    Praktikum dan tugas 1

---
+++ pertemuan1/praktik1/hello.php
@@ -0,0 +1,25 @@
+
+<?php
+    echo "<h1>Hello Word!</h1>";
+    print "<h2>Hello I'm Andrian Nurwana</h2>";
+    
+    $a = 200;
+    $b = 200;
+    $c = 100;
+
+    $h1 = $a + $b - $c ;
+
+    print "h1 = $h1";
+
+    print "<br></br>";
+
+$a= 5; $b=8; $c=4.5;
+ 
+$hasil1=$a+$b;
+$hasil2=$a+$c;
+$hasil3=$a.$b;
+ 
+echo "hasil1 = $hasil1 <br/>"; 
+echo "hasil2 = $hasil2 <br/>"; 
+echo "hasil3 = $hasil3 <br/>"; 
+?>
